1adb5e08a8614f2b54454b4331b88caf2206adeb
Ashish-0016 <ashish.as6021@gmail.com>
2025-03-08 12:33:54 +0530

    fixed cross origin error

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-netlify-app.netlify.app", 
+      "https://search-intresting-recipes.netlify.app/",
     ],
     credentials: true,
   }),
